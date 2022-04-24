0faee0ebbf1b4a97d6c2b6a356d005530e1cbde5
KeuryLendof <lendofkeury@gmail.com>
2022-04-23 23:56:04 -0400

    interfaces

---
+++ src/app/core/interfaces/descuentos.ts
@@ -0,0 +1,8 @@
+export interface IDescuentos{
+    ano: string,
+    mes: string,
+    descripcion: string,
+    monto: string,
+    fechaAplicacion: string,
+    mesStr: string
+}
---
+++ src/app/core/interfaces/logueado.ts
@@ -0,0 +1,18 @@
+export interface ILogueado{
+    id: string,
+    nombre: string,
+    apellido: string,
+    cedula: string,
+    clave: string,
+    email: string,
+    email_p: string,
+    notificar: string,
+    telefono: string,
+    celular: string,
+    direccion: string,
+    f_nacimiento: string,
+    departamento: string,
+    puesto: string,
+    salario: string,
+    token: string
+}
