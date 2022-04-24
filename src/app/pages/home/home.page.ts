b6a1dd0b02f6f94f391e935c1b86f8aa12e38db9
KeuryLendof <lendofkeury@gmail.com>
2022-04-24 19:18:32 -0400

    mensaje de saludo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {IonSlides } from '@ionic/angular';  
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  @ViewChild('slides', { static: true }) slider: IonSlides;
+
+  segment = 0;
+
+  probando = new Date().getHours();
+
+  cuentas = 'block'
+  prestamos = 'display:block'
+
   constructor() { }
 
+  async segmentChanged(ev: any) {  
+    await this.slider.slideTo(this.segment);  
+  }  
+  async slideChanged() {  
+    this.segment = await this.slider.getActiveIndex();  
+  }  
+  
+
   ngOnInit() {
+
+  }
+
+  mostrarCuentas(){
+    this.prestamos = 'display:none'
+  }
+
+  mostrarPrestamos(vista){
+
+    vista = this.prestamos
+    return vista;
+
+  }
+
+  mostrarSaludo(saludo){
+
+    if(saludo<12){
+      return 'Buenos Dias'
+    }
+    if(saludo>12 && saludo<18){
+      return 'Buenas Tardes'
+    }else{
+      return 'Buenas Noches'
+    }
+  }
+
+  mostrarSaludoImagen(hora){
+
+    if(hora<12){
+      return '../../../assets/img/sol.png'
+    }
+    if(hora>12 && hora<18){
+      return '../../../assets/img/atardecer.png'
+    }else{
+      return '../../../assets/img/luna.png'
+    }
   }
 
 }
